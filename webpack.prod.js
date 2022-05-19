bf8456c7b49d0431a3c8eb93ffa215e790de1476
Levon Vardanyan <37041198+vardanyanlg@users.noreply.github.com>
2022-05-19 14:04:10 +0300

    del HtmlInlineScriptPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
 const TerserPlugin = require("terser-webpack-plugin");
-const HtmlInlineScriptPlugin  = require("html-inline-script-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
@@ -20,7 +19,4 @@ module.exports = merge(common, {
       })
     ],
   },
-  plugins: [
-    // new HtmlInlineScriptPlugin()
-  ]
 });
